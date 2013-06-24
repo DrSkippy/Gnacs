cccc2cc57c7f7711e24b6bdaacc5f59ead83140d
Scott Hendrickson <shendrickson@gnip.com>
2013-06-24 01:13:15 +0000

    New package upload

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
     name='gnacs',
-    version='0.5.1',
+    version='0.5.2',
     author='Scott Hendrickson, Josh Montague',
     author_email='scott@drskippy.net',
     packages=['stntvcsv','ggacscsv','diacscsv', 'wpacscsv', 'reflect', 'twacscsv', 'tblracscsv', 'fsqacscsv'],
-    scripts=['wpacs.py','ggacs.py','tblracs.py', 'diacs.py','gnacs-prettifier.py', 'gnacs.py', 'twacs.py', 'fsqacs.py'],
+    scripts=['wpacs.py','ggacs.py','tblracs.py', 'diacs.py', 'gnacs.py', 'twacs.py', 'fsqacs.py'],
     url='https://github.com/DrSkippy27/Gnacs',
     download_url='https://github.com/DrSkippy27/Gnacs/tags/4.0.3',
     license='LICENSE.txt',
