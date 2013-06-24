8ad80efba0d722f94231f66672b0aefed5573e20
Scott Hendrickson <shendrickson@gnip.com>
2013-06-24 01:07:26 +0000

    scripts and version number

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from distutils.core import setup
 
 setup(
     name='gnacs',
-    version='0.5.0',
+    version='0.5.1',
     author='Scott Hendrickson, Josh Montague',
     author_email='scott@drskippy.net',
-    packages=['ggacscsv','diacscsv', 'wpacscsv', 'reflect', 'twacscsv', 'tblracscsv', 'fsqacscsv'],
+    packages=['stntvcsv','ggacscsv','diacscsv', 'wpacscsv', 'reflect', 'twacscsv', 'tblracscsv', 'fsqacscsv'],
     scripts=['ggacs.py','tblracs.py', 'diacs.py','gnacs-prettifier.py', 'gnacs.py', 'twacs.py', 'fsqacs.py'],
     url='https://github.com/DrSkippy27/Gnacs',
     download_url='https://github.com/DrSkippy27/Gnacs/tags/4.0.3',
     license='LICENSE.txt',
-    description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress, Twitter, Tumblr, Foursquare, GetGlue)',
+    description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress, Twitter, Tumblr, Foursquare, GetGlue, StockTwits)',
     install_requires=[
         "ujson >= 1.2",
     ],
