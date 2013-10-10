47ac7122f64f11ee499d0de1e24c35c1d15705e8
Jeff Kolb <jkolb@data-science-1.gnip.com>
2013-10-10 19:38:29 +0000

    added test for pipe-cutoff behavior

--- test/run.bash
+++ test/run.bash
@@ -19,9 +19,11 @@ for pub in $publist; do
     ../gnacs.py -p $fn >> $tmpfile
     echo " GeoJSON $pub from $fn..."
     ../gnacs.py -jgz $pub $fn >> $tmpfile
+    echo " tail-test $pub from $fn..."
+    ../gnacs.py -z $pub $fn | tail >> $tmpfile
 done
 
-echo "Output: $(cat $tmpfile | wc -l) should be 34747"
+echo "Output: $(cat $tmpfile | wc -l) should be 34807"
 echo "GNIP lines: $(cat $tmpfile | grep GNIP | wc -l) should be 1"
 
 if [ -e $tmpfile ]; then
