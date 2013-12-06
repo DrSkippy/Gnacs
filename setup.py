58e82dead821b8fa24425444d1c669c92bd491e3
Scott Hendrickson <shendrickson@gnip.com>
2013-12-06 19:45:51 +0000

    Pip release and new tag

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from distutils.core import setup
 
+GNACS_VERSION_NUMBER = "0.7.5"
+
 setup(
     name='gnacs',
-    version='0.7.3',
+    version=GNACS_VERSION_NUMBER,
     author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman',
     author_email='scott@drskippy.net',
     packages=['acscsv'],
     scripts=['ngacs.py', 'stntv.py', 'wpacs.py', 'ggacs.py','tblracs.py', 'diacs.py', 'gnacs.py', 'twacs.py', 'fsqacs.py'],
     url='https://github.com/DrSkippy27/Gnacs',
-    download_url='https://github.com/DrSkippy27/Gnacs/tags/0.7.3',
+    download_url='https://github.com/DrSkippy27/Gnacs/tags/%s'%(GNACS_VERSION_NUMBER),
     license='LICENSE.txt',
     description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress, Twitter, Tumblr, Foursquare, GetGlue, StockTwits)',
     install_requires=[
