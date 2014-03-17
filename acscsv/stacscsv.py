04f0da05a7e3138b03ffb6fdc82844c326f24f47
Jeff Kolb <jkolb@gnipcentral.com>
2014-03-17 20:51:37 +0000

    Added activity-stream-formatted parsing for stocktwits - native format is selected with "stocktwits-native" - activity-stream format is selected with "stocktwits"

---
+++ acscsv/stacscsv.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__="Jeff Kolb"
+__license__="Simplified BSD"
+import sys
+import acscsv
+
+class StacsCSV(acscsv.AcsCSV):
+    def __init__(self, delim, options_user,options_struct,options_influence):
+        super(StacsCSV, self).__init__(delim)
+        self.options_user = options_user
+        self.options_struct = options_struct
+        self.options_influence = options_influence
+
+    def procRecordToList(self, d):
+        record = []
+        try:
+            record.append(d['object']['id'])
+            record.append(d['object']['postedTime'])
+            record.append(self.cleanField(d['object']['summary']))
+            
+            # TODO
+            #if self.options_struct:
+            #    in_reply_to = "None"
+            #    parent_message = "None"
+            #    replies = "None"
+            #    if "conversation" in d:
+            #        con = d["conversation"]
+            #        if "in_reply_to_message_id" in con:
+            #            in_reply_to = str(con["in_reply_to_message_id"])
+            #        if "parent_message_id" in con:
+            #            parent_message = str(con["parent_message_id"])
+            #        if "replies" in con:
+            #            replies = str(con["replies"])
+            #    record.append(parent_message)
+            #    record.append(in_reply_to)
+            #    record.append(replies)
+            #
+            if self.options_user:
+                website_url = 'None'
+                actor = d['actor']
+                record.append(self.cleanField(actor['preferredUsername']))
+                record.append(self.cleanField(actor['displayName']))
+                if actor['link'] != None:
+                    website_url = actor['link']
+                record.append(website_url)
+            #
+            if self.options_influence:
+                following_stocks = str(actor['followingStocksCount'])
+                followers = str(actor['followingCount'])
+                experience = str(actor['trading_strategy']['experience'])
+                record.append(following_stocks)
+                record.append(followers)
+                record.append(experience)
+            return record
+        except KeyError, e:
+            sys.stderr.write("Field missing from record (%d), skipping\n"%self.cnt)
+            sys.stderr.write("(%s)\n" % e)
+            record.append(acscsv.gnipError)
+            record.append(acscsv.gnipRemove)
+            return record
