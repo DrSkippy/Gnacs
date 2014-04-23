a75f16aed125ff7e4d08e8581ce0420e588efcec
Josh Montague <joshua.montague@gmail.com>
2014-04-22 18:10:26 -0600

    WIP - continue work on refactor

--- acscsv/new_twacs.py
+++ acscsv/new_twacs.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-__author__="Scott Hendrickson"
+__author__="Scott Hendrickson, Josh Montague"
 __license__="Simplified BSD"
 import sys
 import acscsv
@@ -8,6 +8,12 @@ import inspect
 # move to acscsv?
 
 class _field(object):
+    """
+    Base class for finding the appropriate key-value pairs in a JSON Activity Streams payload.
+    Common default value (for e.g. missing values) is "None", but child subclasses can override
+    this default when needed. Subclasses should also define the keypath to the desired location
+    by overwriting the "path" list. 
+    """
     default_value = "None"
     value = None
     path = []
@@ -26,16 +32,101 @@ class _field(object):
             res = res[k]
         return res
 
-# parsing and formating objects 
+#### parsing and formating objects 
+# 2014-04-22, JM
+#   -- working through them top --> bottom of original twacscsv.py
+
 class field_verb(_field):
+    """assign to self.value the value of top-level 'verb' key"""
+    # specify path to desired field as a list of dict keys 
     path = ["verb"]
+    # if needed, overwrite default_value
     default_value = "Unidentified meta message"
     
     def __init__(self, json_record):
+        """Calls base constructor, which walks the specified dict path to find appropriate value"""
         super(
                 field_verb
                 , self).__init__(json_record)
-        # custom parsing here
+        # in any field_* class, add any needed custom parsing here
+
+
+class field_gnip_urls(_field):
+    """assign to self.value the list of 'expanded_url' values within 'gnip', 'urls'"""
+    # nb: there exists a t.co url
+    path = ["gnip", "urls"]
+    
+    def __init__(self, json_record):
+        super(
+            field_gnip_urls
+            , self).__init__(json_record)
+        # after base __init__(), self.value is a list of url & expanded url dicts.
+        # call buildListString within the Twacs class, not here. just return the list.
+        self.value = [ x["expanded_url"] for x in self.value ] ]
+
+
+class _field_twitter_urls(_field):
+    """base class for accessing multiple url fields within 'twitter_entities', 'urls'"""
+    # nb: there are 3x urls in twitter_entities 
+    path = ["twitter_entities", "urls"]
+    
+    def __init__(self, json_record, key=None):
+        super(
+            _field_twitter_urls
+            , self).__init__(json_record)
+        # after base __init__(), self.value is a list. can be empty or list of dicts 
+        if key is not None and isinstance(self.value, list) and len(self.value) > 0:
+            for d in self.value:
+                try:
+                    # JM: previous TypeError exception in case d isn't a dict? 
+                    if key in d["urls"] and d["urls"][key] is not None: 
+                        url_list.append( d["urls"][key] )
+                    else:
+                        url_list.append( self.default_value ) 
+                except TypeError:
+                    url_list = [ self.default_value ]
+            self.value = url_list
+        else:
+            self.value = [ self.default_value ]
+
+
+class field_twitter_urls_url(_field_twitter_urls):
+    """assign to self.value the list of 'url' values within 'twitter_entities', 'urls' dict"""
+    
+    def __init__(self, json_record):
+        super(
+            field_twitter_urls_url
+            , self).__init__(json_record, key="url")
+
+
+class field_twitter_expanded_urls(_field_twitter_urls):
+    """assign to self.value the list of 'expanded_url' values within 'twitter_entities', 'urls' dict"""
+
+    def __init__(self, json_record):
+        super(
+            field_twitter_expanded_urls
+            , self).__init__(json_record, key="expanded_url")
+
+
+class field_twitter_display_urls(_field):
+    """assign to self.value the list of 'display_url' values within 'twitter_entities', 'urls' dict"""
+
+    def __init__(self, json_record):
+        super(
+            field_twitter_expanded_urls
+            , self).__init__(json_record, key="display_url")
+
+
+class field_actor_lang(_field):
+    """assign to self.value the value of 'actor', 'languages'"""
+    path = ["actor", "languages"]
+    
+    def __init__(self, json_record):
+        super(
+            field_actor_lang
+            , self).__init__(json_record)
+        # self.value is now a list
+
 
 class field_gnip_language(_field):
     path = ["gnip","language","value"]
@@ -44,7 +135,21 @@ class field_gnip_language(_field):
         super(
                 field_gnip_language
                 , self).__init__(json_record)
-        # custom parsing here
+
+
+class field_twitter_lang(_field):
+    """assign to self.value the value of top-level 'twitter_lang'"""
+    path = ["twitter_lang"]
+    
+    def __init__(self, json_record):
+        super(
+            field_twitter_lang
+            , self).__init__(json_record)
+        # JM, does twitter_lang ever have > 1 value?
+
+#################
+# WIP
+################
 
 
 
@@ -69,12 +174,17 @@ class Twacs(acscsv.AcsCSV):
         self.options_influence = options_influence
         self.options_struct = options_struct
         
-    def procRecordToList(self,d):
+    def procRecordToList(self, d):
+        """
+        Reads JSON payload d, identify the activity type by its verb, 
+        return the appropriate parsed + delimited line.
+        """ 
         record = []
-        try:
+        try:        # try block needs a KeyError exception?
             #if "verb" in d:
             #    verb = d["verb"]
             verb = field_verb(d).value
+            # check for compliance
             if verb is None:
                 msg = "Unidentified meta message"
                 for mtype in ["error", "warning", "info"]:
@@ -99,16 +209,18 @@ class Twacs(acscsv.AcsCSV):
                 record.append(acscsv.gnipDateTime)
                 record.append('-'.join([acscsv.gnipRemove, verb]))
                 return record
+        # at this point, it's a valid, non-compliance activity
         return self.csv(d, record)
 
+    # refactor to use objects
     def csv(self, d, record)
-            # refactor to use objects
-            # always first 2 items
+        try:                            # KeyError exception at EOF
+            # always first 2 items 
             record.append(d["id"])
             record.append(d["postedTime"])
             # add some handling so calling gnacs without a pub is still useful
-            #   n.b.: -stocktwits is native, so no 'verb' to get here
-            #         -put more-specific fields at the beginning to catch them 
+            #   -put more-specific fields at the beginning to catch them 
+            #   -stocktwits is native, so no 'verb' to get here
             obj = d["object"]
             if d["id"].rfind("getglue") != -1 : # getglue 
                 record.append(verb)     # 'body' is inconsistent in gg
@@ -125,53 +237,76 @@ class Twacs(acscsv.AcsCSV):
             else:                       # ? 
                 record.append("None")
             #
-            gnip = {}
-            actor = {}
-            if "gnip" in d:
-                gnip = d["gnip"]
-            if "actor" in d:    # no 'actor' in ng
-                actor = d["actor"]
+#            gnip = {}
+#            actor = {}
+#            if "gnip" in d:
+#                gnip = d["gnip"]
+#            if "actor" in d:    # no 'actor' in ng
+#                actor = d["actor"]
             #
+            # JM: breaking this section out into separate classes, 
+            #       but the if self.options flag will combine them all (consistent 
+            #       current implementation
             if self.options_urls:
-                urls = "None"
-                if "urls" in gnip:
-                    urls = self.buildListString([ l["expanded_url"] for l in gnip["urls"]])
-                record.append(urls)
-                twitter_urls = "None"
-                twitter_un_urls = "None"
-                if "twitter_entities" in d:
-                    if "urls" in d["twitter_entities"]:
-                        if len(d["twitter_entities"]["urls"]) > 0:
-                            tmp_url_list = []
-                            tmp_un_url_list = []
-                            for tmp_rec in d["twitter_entities"]["urls"]:
-                                try:
-                                    if "url" in tmp_rec and tmp_rec["url"] is not None:
-                                        tmp_url_list.append(tmp_rec["url"])
-                                    else:
-                                        tmp_url_list.append("None")
-                                except TypeError:
-                                    tmp_url_list = ["twitter_entiteis:urls:url"]
-                                try:
-                                    if "expanded_url" in tmp_rec and tmp_rec["expanded_url"] is not None:
-                                        tmp_un_url_list.append(tmp_rec["expanded_url"])
-                                    else:
-                                        tmp_un_url_list.append("None")
-                                except TypeError:
-                                    tmp_un_urls_list = ["twitter_entities:urls:expanded_url"]
-                            twitter_urls = self.buildListString(tmp_url_list)
-                            twitter_un_urls = self.buildListString(tmp_un_url_list)
-                record.append(twitter_urls)
-                record.append(twitter_un_urls)
+                ## new code, JM
+                record.append( self.buildListString( field_gnip_urls(d).value ) ) 
+                record.append( self.buildListString( field_twitter_urls_url(d).value ) ) 
+                record.append( self.buildListString( field_twitter_urls_expanded_url(d).value ) ) 
+                record.append( self.buildListString( field_twitter_urls_display_url(d).value ) ) 
+                ##
+#                urls = "None"
+#                if "urls" in gnip:
+#                    urls = self.buildListString([ l["expanded_url"] for l in gnip["urls"]])
+#                record.append(urls)
+#                twitter_urls = "None"
+#                twitter_un_urls = "None"
+#                if "twitter_entities" in d:
+#                    if "urls" in d["twitter_entities"]:
+#                        if len(d["twitter_entities"]["urls"]) > 0:
+#                            tmp_url_list = []
+#                            tmp_un_url_list = []
+#                            for tmp_rec in d["twitter_entities"]["urls"]:
+#                                try:
+#                                    if "url" in tmp_rec and tmp_rec["url"] is not None:
+#                                        tmp_url_list.append(tmp_rec["url"])
+#                                    else:
+#                                        tmp_url_list.append("None")
+#                                except TypeError:
+#                                    tmp_url_list = ["twitter_entiteis:urls:url"]
+#                                try:
+#                                    if "expanded_url" in tmp_rec and tmp_rec["expanded_url"] is not None:
+#                                        tmp_un_url_list.append(tmp_rec["expanded_url"])
+#                                    else:
+#                                        tmp_un_url_list.append("None")
+#                                except TypeError:
+#                                    tmp_un_urls_list = ["twitter_entities:urls:expanded_url"]
+#                            twitter_urls = self.buildListString(tmp_url_list)
+#                            twitter_un_urls = self.buildListString(tmp_un_url_list)
+#                record.append(twitter_urls)
+#                record.append(twitter_un_urls)
             if self.options_lang:
-                try:
-                    record.append(str([str(l) for l in actor["languages"]]))
+                ## new code, JM
+                # actor lang
+                try: 
+                    record.append( self.buildListString( field_actor_lang(d).value ) ) 
                 except UnicodeEncodeError, e:
-                    record.append(str("bad encoding"))
+                    record.append("bad-encoding")
+                # gnip lang
+                record.append(field_gnip_language(d).value)
+                # twitter lang
+                 
+########################################
+# WIP here, JM
+########################################
+
+                ##
+#                try:
+#                    record.append(str([str(l) for l in actor["languages"]]))
+#                except UnicodeEncodeError, e:
+#                    record.append(str("bad encoding"))
                 #glang = "None"
                 #if "language" in gnip:
                 #    glang = gnip["language"]["value"]
-                record.append(field_gnip_language(d).value)
                 tlang = "None"
                 if "twitter_lang" in d:
                     tlang = d["twitter_lang"]
@@ -314,6 +449,7 @@ class Twacs(acscsv.AcsCSV):
             return record
 
     def db_output(self, d):
+        """Explicitly specific the order and fields that get output to specific files."""
         return [
                 field_gnip_language(d).value, 
                 ]
