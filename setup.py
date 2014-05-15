66c6dcbe767f627707a01edc9457e5d903281cb1
Scott Hendrickson <scott@drskippy.net>
2014-05-15 13:34:19 -0600

    Deprecated utility command line functions

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if '__main__' == __name__:
         author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman',
         author_email='scott@drskippy.net',
         packages=['acscsv'],
-        scripts=['ngacs.py', 'stntv.py', 'wpacs.py', 'ggacs.py','tblracs.py', 'diacs.py', 'gnacs.py', 'twacs.py', 'fsqacs.py'],
+        scripts=['gnacs.py'],
         url='https://github.com/DrSkippy27/Gnacs',
         download_url='https://github.com/DrSkippy27/Gnacs/tags/%s'%(GNACS_VERSION_NUMBER),
         license='LICENSE.txt',
