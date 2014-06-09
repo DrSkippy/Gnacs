5680ccd4191800fdedf33db0169356d21baf7302
Josh Montague <joshua.montague@gmail.com>
2014-06-09 18:17:01 +0000

    move acscsv testing code to test_acscsv module

--- acscsv/acscsv.py
+++ acscsv/acscsv.py
@@ -5,7 +5,6 @@ __license__="Simplified BSD"
 import sys
 import datetime
 import json
-import unittest
 
 gnipError = "GNIPERROR"
 gnipRemove = "GNIPREMOVE"
@@ -134,25 +133,6 @@ class _limited_field(_field):
 # - replace 2-level dict traversal (eg profileLocation base class) with acscsv.walk_path() or 
 #       similar helper method 
 
-
-class TestAcsCSV(unittest.TestCase):
-    """Unit tests ofr common CSV utility functions"""
-    def setUp(self):
-        pass
-
-    def tearDown(self):
-        pass
-
-    def testCleanField(self):
-        a = AcsCSV("|", False)
-        self.assertEquals(a.cleanField("laksjflasjdfl;a"), "laksjflasjdfl;a")
-        self.assertEquals(a.cleanField("\r\n \n\r \r\r \n\n"), "")
-        self.assertEquals(a.cleanField("\r\na \n\r \r\r a\n\n"), "a       a")
-        self.assertEquals(a.cleanField("asdf|asdf,,adsf|asdf"), "asdf asdf,,adsf asdf")
-        b = AcsCSV(",", False)
-        self.assertEquals(b.cleanField("asdf|asdf,,adsf|asdf"), "asdf|asdf  adsf|asdf")
-        self.assertEquals(b.cleanField(245), "245")
-        self.assertEquals(b.cleanField(a), "None")
 
 class AcsCSV(object):
     """Base class for all delimited list objects. Basic delimited list utility functions"""
---
+++ acscsv/test_acscsv.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+__author__="Scott Hendrickson"
+__license__="Simplified BSD"
+
+import unittest
+from acscsv import *  
+
+class TestAcsCSV(unittest.TestCase):
+    """Unit tests ofr common CSV utility functions"""
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def testCleanField(self):
+        a = AcsCSV("|", False)
+        self.assertEquals(a.cleanField("laksjflasjdfl;a"), "laksjflasjdfl;a")
+        self.assertEquals(a.cleanField("\r\n \n\r \r\r \n\n"), "")
+        self.assertEquals(a.cleanField("\r\na \n\r \r\r a\n\n"), "a       a")
+        self.assertEquals(a.cleanField("asdf|asdf,,adsf|asdf"), "asdf asdf,,adsf asdf")
+        b = AcsCSV(",", False)
+        self.assertEquals(b.cleanField("asdf|asdf,,adsf|asdf"), "asdf|asdf  adsf|asdf")
+        self.assertEquals(b.cleanField(245), "245")
+        self.assertEquals(b.cleanField(a), "None")
+
+
+if __name__ == "__main__":
+    unittest.main()
