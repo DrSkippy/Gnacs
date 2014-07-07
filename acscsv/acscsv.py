8e263586dce8fa59c594a5a5e126b9489172aa74
Jeff Kolb <jkolb@twitter.com>
2014-07-07 20:38:52 +0000

    added label attr to acscsv._Field

--- acscsv/acscsv.py
+++ acscsv/acscsv.py
@@ -40,7 +40,11 @@ class _Field(object):
     value_list = [ default_value ]  # overwrite when value is most appropriately a list 
     path = []                       # dict key-path to follow for desired value
 
+    label = 'DummyKeyPathLabel'         # this must match if-statement in constructor
+
     def __init__(self, json_record):
+        if self.label == 'DummyKeyPathLabel':
+            self.label = ':'.join(self.path)
         self.value = self.walk_path(json_record)
 
     def __repr__(self):
@@ -93,6 +97,8 @@ class _Field(object):
 
 
 class _LimitedField(_Field):
+    #TODO: is there a better way that this class and the fix_length() method in _Field class
+    #       could be combined?
     """  
     Takes JSON record (in python dict form) and optionally a maximum length (limit, 
     with default length=5). Uses parent class _Field() to assign the appropriate value 
@@ -106,9 +112,6 @@ class _LimitedField(_Field):
     to overwrite the fields list ( fields=["a", "b"] ) to obtain this result. 
     Finally, self.value is set to a string representation of the final self.value_list.
     """
-    #
-    #TODO: move this to the mysql test module? I think it was only used there. 
-    #
     fields = None 
     
     #TODO: set limit=None by default and just return as many as there are, otherwise (by specifying 
