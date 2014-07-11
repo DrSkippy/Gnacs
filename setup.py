de44918243449c8fe520e7788db0dd15a7e71502
Scott Hendrickson <drskippy@twitter.com>
2014-07-11 17:54:57 +0000

    Typos in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from distutils.core import setup
 
-GNACS_VERSION_NUMBER = "1.0.0"
+GNACS_VERSION_NUMBER = "1.0.1"
 
 if '__main__' == __name__:
     setup(
         name='gnacs',
         version=GNACS_VERSION_NUMBER,
-        author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman',
+        author='Scott Hendrickson, Josh Montague, Jinsub Hong, Jeff Kolb, Brian Lehman, Fiona Pigott',
         author_email='drskippy@twitter.com',
         packages=['acscsv'],
         scripts=['gnacs.py'],
         url='https://github.com/DrSkippy27/Gnacs',
         download_url='https://github.com/DrSkippy27/Gnacs/tags/%s'%(GNACS_VERSION_NUMBER),
         license='LICENSE.txt',
-        description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress Posts and COmments, Twitter, Tumblr, Foursquare, StockTwits)',
+        description='Gnip normalized activity JSON to csv parser (Disqus Comments, Wordpress Posts and Comments, Twitter, Tumblr, Foursquare, StockTwits)',
         install_requires=[
                     "ujson >= 1.2",
                         ]
